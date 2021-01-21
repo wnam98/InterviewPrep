806e4f93f73029444c47ac68d1fd03d255e8797e
Walter Nam <49884140+wnam98@users.noreply.github.com>
2021-01-21 16:44:26 +0900

    Minimum Path Sum

--- Solution.java
+++ Solution.java
@@ -90,6 +90,34 @@ public class Solution{
             }
         }return minHeap.remove();
     }
+
+    /***
+     *<p>
+     *     Explanation: Greedy Algorithm solution. Traverse the matrix, and at
+     *     each step find the optimal minimum and append to new DP 2D array.
+     *</p>
+     * @param grid Takes in an M by N grid containing ints
+     * @return Minimum sum from top left to bottom right
+     */
+    public int minPathSum(int[][] grid){
+        if(grid == null || grid.length == 0) return 0;
+        int dp[][] = new int[grid.length][grid[0].length];
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                dp[i][j] += grid[i][j];
+                if(i > 0 && j > 0){
+                    dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }else if(i > 0){
+                    dp[i][j] += dp[i - 1][j];
+                }else if(j > 0){
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[grid.length-1][grid[0].length-1];
+    }
     //------------------------------Binary Tree Questions-----------------------------------------------------------//
 
     /***
