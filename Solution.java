ce6ea61dbaea8ed7ac211fe17cbf1acd01ee8be6
Walter Nam <49884140+wnam98@users.noreply.github.com>
2021-01-27 15:51:30 +0900

    N Queens LeetCode Hard

--- Solution.java
+++ Solution.java
@@ -157,6 +157,51 @@ public class Solution{
                 wordHelperDFS(board, i - 1, j, count + 1, word) ||
                 wordHelperDFS(board, i, j - 1, count + 1, word);
     }
+
+    //min queens problem
+    List<List<String>> res = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        placeQueen(new int[n][n], 0, new HashSet<>(), new HashSet<>(), new HashSet<>());
+        return res;
+    }
+
+    //recursive helper function
+    public void placeQueen(int[][] board, int i, Set<Integer> diagonal, Set<Integer> diagonal2, Set<Integer> vertical){
+        if(i == board.length){
+            addToList(board);
+            return;
+        }
+
+        for(int j = 0; j < board.length; j++){
+            if(!diagonal.contains(i + j) && !diagonal2.contains(j - i) && !vertical.contains(j)){
+                board[i][j] = 1;
+                diagonal.add(i + j);
+                diagonal2.add(j - i);
+                vertical.add(j);
+                placeQueen(board, i + 1, diagonal, diagonal2, vertical);
+                board[i][j] = 0;
+                diagonal.remove(i + j);
+                diagonal2.remove(j - i);
+                vertical.remove(j);
+            }
+        }
+    }
+
+    public void addToList(int[][] board){
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < board.length; i++){
+            String temp = "";
+            for(int j = 0; j < board.length; j++){
+                if(board[i][j] == 0){
+                    temp += ".";
+                }else{
+                    temp += "Q";
+                }
+            }
+            list.add(temp);
+        }
+        res.add(list);
+    }
     //------------------------------Binary Tree Questions-----------------------------------------------------------//
 
     /***
