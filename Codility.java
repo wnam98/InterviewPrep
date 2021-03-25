6aeaa0d6e51fd5de787bf23cd155bf1cdef66da2
Walter Nam <49884140+wnam98@users.noreply.github.com>
2021-03-25 09:20:06 +0900

    added rotatearray codility

--- Codility.java
+++ Codility.java
@@ -41,8 +41,64 @@ public class Codility {
 		}
 		return max;
     }
+
+    //---------------------------Chapter 2: Arrays-------------------------------------------//
+
+	/***
+	 * An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+	 * The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+	 * Write a function:
+	 * class Solution { public int[] solution(int[] A, int K); }
+	 * that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+	 * For example, given
+	 *     A = [3, 8, 9, 7, 6]
+	 *     K = 3
+	 * the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+	 *     [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+	 *     [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+	 *     [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+	 * For another example, given
+	 *     A = [0, 0, 0]
+	 *     K = 1
+	 * the function should return [0, 0, 0]
+	 *
+	 * Given
+	 *
+	 *     A = [1, 2, 3, 4]
+	 *     K = 4
+	 * the function should return [1, 2, 3, 4]
+	 *
+	 * Assume that:
+	 *
+	 * N and K are integers within the range [0..100];
+	 * each element of array A is an integer within the range [−1,000..1,000].
+	 * In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+	 *
+	 * ***/
+
+	public int[] rotateArray(int[] A, int K){
+		K = K % A.length;
+		shift(0, A.length - 1, A);
+		shift(0, K - 1, A);
+		shift(K, A.length - 1, A);
+		return A;
+	}
+
+	public void shift(int start, int end, int[] array){
+		while(start < end){
+			int temp = array[start];
+			array[start] = array[end];
+			array[end] = temp;
+			start++;
+			end--;
+		}
+	}
+
+    //Driver method
     public static void main(String[] args) {
         Codility solution = new Codility();
         System.out.println(solution.binaryGap(100));
+        int[] test = {3, 8, 9, 7, 6};
+		System.out.println(Arrays.toString(solution.rotateArray(test, 3)));
     }
 }
